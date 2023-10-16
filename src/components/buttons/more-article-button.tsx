53596241eb2914ef6d330b6dfdb00944376ec3b6
NeterAlex <neteralex@outlook.com>
2023-10-16 19:38:27 +0800

    feat: updated

--- src/components/buttons/more-article-button.tsx
+++ src/components/buttons/more-article-button.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@nextui-org/react";
 import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
 
 export default function MoreArticleButton() {
   const router = useRouter();
+  const { theme } = useTheme();
   return (
     <>
       <div className="flex flex-col justify-center pb-5">
         <Button
           className="header-font w-full"
-          color="secondary"
+          color={theme === "light" ? "secondary" : "primary"}
           variant="light"
           radius="none"
           onClick={() => void router.push("/article")}
